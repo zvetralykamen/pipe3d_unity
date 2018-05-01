03795663521760043eebcba25f3a4880f0be44bd
zvetraly_kamen <bergoss37@gmail.com>
2018-05-01 11:07:03 +0200

    Random flow start point in the grid + minor visual change.

---
+++ pipe3d_unity/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSpace : MonoBehaviour {
+
+	public bool isEmpty = true;
+	public bool isStartingPoint = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ pipe3d_unity/Assets/Scripts/LevelManagment.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManagment : MonoBehaviour {
+
+	public GameObject gridSpace;
+
+	public int levelX;
+	public int levelY;
+	public int levelZ;
+
+	// Use this for initialization
+	void Start () {
+		InitializeGridSpace ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void InitializeGridSpace(){
+		Vector3 currentVector = Vector3.zero; 
+
+		//Random starting position of the flow.
+		Vector3 startingSpace = new Vector3 (Random.Range (0, levelX), Random.Range (0, levelY), Random.Range (0, levelZ));
+		Debug.Log (startingSpace);
+
+		//Generating grid.
+		for (var i = 0; i < levelY; i++) {
+			for (var j = 0; j < levelZ; j++) {
+				for (var k = 0; k < levelX; k++) {
+					currentVector = new Vector3 (k, i, j);
+					GameObject gs_object = Instantiate (gridSpace, currentVector, Quaternion.identity);
+					GridSpace gs = gs_object.GetComponentInChildren<GridSpace>();
+
+					//Find starting space.
+					if (currentVector == startingSpace) {
+						Debug.Log ("Starting space found!");
+						gs.isStartingPoint = true;
+						gs.isEmpty = false;
+						gs_object.GetComponentInChildren<Renderer> ().material.color = Color.cyan;
+					}
+				}
+			}
+		}
+	}
+}
