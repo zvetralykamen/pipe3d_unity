0e0cec0f960816cf2d1362468c9c33d43f56a9eb
zvetraly_kamen <bergoss37@gmail.com>
2018-05-01 21:59:21 +0200

    Started working on hiding/unhiding layers of grid (ViewController).

---
+++ pipe3d_unity/Assets/Scripts/ViewController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewController : MonoBehaviour {
+
+	//Get LevelManagment script.
+	public LevelManagment lm;
+
+	private BoxCollider	boxcol;
+	private Vector3 currentcenter;
+
+	// Use this for initialization
+	void Start () {
+		boxcol = gameObject.GetComponent<BoxCollider> ();
+		boxcol.size = new Vector3 (lm.levelX, lm.levelY, lm.levelZ);
+		currentcenter = new Vector3 (lm.levelX / 2, lm.levelY + lm.levelY / 2, lm.levelZ / 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		boxcol.center = currentcenter;
+
+		if (Input.GetKeyDown(KeyCode.O)) {
+			currentcenter = new Vector3(currentcenter.x,currentcenter.y + 1,currentcenter.z);
+		}
+		if (Input.GetKeyDown(KeyCode.P)) {
+			currentcenter = new Vector3(currentcenter.x,currentcenter.y - 1,currentcenter.z);
+		}
+		
+	}
+}
