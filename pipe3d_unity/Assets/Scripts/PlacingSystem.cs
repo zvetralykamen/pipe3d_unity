220a92a35a46f07add013c88ab6c95a4af41d3a8
Xientra <38855570+Xientra@users.noreply.github.com>
2018-05-07 20:09:06 +0200

    Placing some given pipes but not with fancy drag and drop...
    Took me long enouth to make them have random rotations...
    I changed the Layer of ViewController to IgnoreRaycast hope that does't interfear with something.

---
+++ pipe3d_unity/Assets/Scripts/PlacingSystem.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacingSystem : MonoBehaviour {
+
+    public Camera camera1; 
+    
+    
+    //pipe prefabs
+    public GameObject pipe_straight; //1
+    /*
+    public GameObject pipe_curve; //2
+    public GameObject pipe_T; //3
+    public GameObject pipe_cross; //4
+    public GameObject pipe_cross_plus1; //5 // I don't know how to call it; it's a cross but with one additional connection up or down
+    public GameObject pipe_allconnected; //6
+    */
+    public GameObject pipe_end; //7
+
+    //vars for placing options
+    public int amount_of_options = 7;
+    public float[] rotations = new float[] { 0, 90, 180, 270 };
+    public GameObject option_1;
+    public GameObject option_2;
+    public GameObject option_3;
+    private bool option_1_available = true;
+    private bool option_2_available = true;
+    private bool option_3_available = true;
+
+    //vars for 
+    private bool selecting = true;
+    private GameObject selected;
+
+    void Start() {
+    }
+
+    void Update() {
+        setOptions(); //random rotation is n- ... i don't know i think it works now...
+
+        movingOptions();
+    }
+
+    void setOptions() {
+        if (option_1_available) {
+            instantiate_rndOption(option_1.transform.position);
+            option_1_available = false;
+        }
+        else if (option_2_available) {
+            instantiate_rndOption(option_2.transform.position);
+            option_2_available = false;
+        }
+        else if (option_3_available) {
+            instantiate_rndOption(option_3.transform.position);
+            option_3_available = false;
+        }
+    }
+
+    void instantiate_rndOption(Vector3 position) {
+        int rnd = 1; //int rnd = Random.Range(1, amount_of_options);
+        Quaternion rndRotation = Quaternion.Euler(rotations[Random.Range(0, 3)], rotations[Random.Range(0, 3)], rotations[Random.Range(0, 3)]);
+
+        switch (rnd) {
+            case 1:
+                Instantiate(pipe_straight, position, rndRotation);
+                break;
+               /*
+            case 2:
+                Instantiate(pipe_curve, position, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(pipe_T, position, Quaternion.identity);
+                break;
+            case 4:
+                Instantiate(pipe_cross, position, Quaternion.identity);
+                break;
+            case 5:
+                Instantiate(pipe_cross_plus1, position, Quaternion.identity);
+                break;
+            case 6:
+                Instantiate(pipe_allconnected, position, Quaternion.identity);
+                break;
+                */
+            case 7:
+                Instantiate(pipe_end, position, Quaternion.identity);
+                break;
+        } 
+    }
+
+    void movingOptions() { //I had to change _ViewController's Layer to 2/IgnoreRaycast bc the ray didn't went throught it's collider
+
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray1 = camera1.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitinfo;
+            if (Physics.Raycast(ray1, out hitinfo)) {
+                Debug.Log(hitinfo.transform.gameObject.name);
+
+                if (selecting == true) {
+                    if (hitinfo.transform.gameObject.CompareTag("Pipe")) {
+                        selected = hitinfo.transform.gameObject;
+                        selecting = false;
+                        Debug.Log("Selected: " + selected.name);
+                    }
+                }
+                else if (selecting == false) {
+                    if (hitinfo.transform.gameObject.CompareTag("GridSpace")) {
+                        Destroy(selected);
+                        Instantiate(selected, hitinfo.transform.position, selected.transform.rotation);
+                        Destroy(hitinfo.transform.gameObject);
+                        selected = null;
+                        selecting = true;
+                    }
+                }
+            }
+        }
+    }
+}
